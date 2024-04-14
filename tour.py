d88ff716cf3dba041c954ffbdeba631f5c98fd1f
Chaithanya S <84765756+chaithanya762@users.noreply.github.com>
2024-04-14 14:27:31 +0530

    Update tour.py

--- tour.py
+++ tour.py
@@ -43,20 +43,20 @@ st.write("## Image Gallery")
 
 # Mysore Attractions Images
 images = [
-    ("Mysore Palace", "https://cdn.pixabay.com/photo/2015/12/02/14/54/mysore-palace-1079933_960_720.jpg"),
-    ("Chamundi Hills", "https://cdn.pixabay.com/photo/2016/01/08/05/09/chamundi-hills-1129608_960_720.jpg"),
-    ("Brindavan Gardens", "https://cdn.pixabay.com/photo/2014/12/15/13/08/brindavan-gardens-569296_960_720.jpg"),
-    ("St. Philomena Church", "https://cdn.pixabay.com/photo/2017/11/16/21/35/st-philomena-church-2951443_960_720.jpg"),
-    ("Mysore Zoo", "https://cdn.pixabay.com/photo/2017/05/10/07/43/mysore-2308128_960_720.jpg"),
-    ("Sri Chamarajendra Park", "https://cdn.pixabay.com/photo/2015/12/02/14/52/sri-chamarajendra-park-1079931_960_720.jpg"),
-    ("Jaganmohan Palace", "https://cdn.pixabay.com/photo/2015/08/10/01/24/jaganmohan-palace-882128_960_720.jpg"),
-    ("Rail Museum", "https://cdn.pixabay.com/photo/2018/01/19/18/17/indian-railway-museum-3094770_960_720.jpg"),
-    ("Karanji Lake", "https://cdn.pixabay.com/photo/2014/12/09/22/25/karanji-lake-562314_960_720.jpg"),
-    ("KRS Dam", "https://cdn.pixabay.com/photo/2017/12/04/16/43/krishna-raja-sagara-2992020_960_720.jpg"),
-    ("Jayalakshmi Vilas Mansion", "https://cdn.pixabay.com/photo/2016/09/29/11/38/india-1700112_960_720.jpg"),
-    ("Mysore Urban Development Authority (MUDA)", "https://cdn.pixabay.com/photo/2015/03/17/14/05/river-677442_960_720.jpg"),
-    ("St. Thomas Church", "https://cdn.pixabay.com/photo/2017/12/11/14/07/st-thomas-church-3013465_960_720.jpg"),
-    ("Mysore Race Course", "https://cdn.pixabay.com/photo/2014/11/06/15/09/mysore-race-course-520219_960_720.jpg")
+    ("Mysore Palace", "https://images.app.goo.gl/geGQirfLz9gFqrhU9"),
+    ("Chamundi Hills", "https://images.app.goo.gl/iiunDd2qEwWTgLmb6"),
+    ("Brindavan Gardens", "https://images.app.goo.gl/Umi9X4EQ1fVEV7RCA"),
+    ("St. Philomena Church", "https://images.app.goo.gl/cG5WbsEcREn26T4e9"),
+    ("Mysore Zoo", "https://images.app.goo.gl/CyMrWiuczm2UTMms9"),
+    ("Sri Chamarajendra Park", "https://images.app.goo.gl/FQrZx4d3ZNfuQcDK6"),
+    ("Jaganmohan Palace", "https://images.app.goo.gl/Uzu8DVuUGgCqm4BA8"),
+    ("Rail Museum", "https://images.app.goo.gl/esXMyEaer6d4AwWcA"),
+    ("Karanji Lake", "https://images.app.goo.gl/76Cpm4VepedThfPi7"),
+    ("KRS Dam", "https://images.app.goo.gl/HToQ9UqTPbZdFAaEA"),
+    ("Jayalakshmi Vilas Mansion", "https://images.app.goo.gl/DLtj1dbu1jQTCPo69"),
+    ("Mysore Urban Development Authority (MUDA)", "https://images.app.goo.gl/LcfQVb6j6XmKzgFRA"),
+    ("Nanjangud", "https://images.app.goo.gl/ZdwQdnwJPQNv5dMMA"),
+    ("Mysore Race Course", "https://images.app.goo.gl/TYMJWKvs9Stuw5T76")
 ]
 
 # Display Images
