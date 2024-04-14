ad568c26325d768a3218636f0bcfe53885edf2be
Chaithanya S <84765756+chaithanya762@users.noreply.github.com>
2024-04-14 15:28:44 +0530

    Update tour.py

--- tour.py
+++ tour.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Set page configuration to dark mode
 st.set_page_config(layout="wide", page_title="Mysore Tourism", page_icon=":cityscape:")
-background_image_url = "https://t4.ftcdn.net/jpg/01/85/45/39/360_F_185453991_NKQieCp6yWywyYkAjX0O3FtQSwdHxJTB.jpg"
+background_image_url = "https://img.freepik.com/premium-photo/blurred-light-purple-pink-background-defocused-art-abstract-lilac-gradient-backdrop-with-blur-bokeh-blurry-wallpaper_113767-5465.jpg"
 background_image_style = f"""
     <style>
     .stApp {{
